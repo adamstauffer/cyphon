39e0c3c7f67d0e9689f47442a48e817bca737fcc
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2017-03-27 17:51:04 -0400

    bug fix

--- django/target/followees/models.py
+++ django/target/followees/models.py
@@ -39,7 +39,7 @@ class Followee(models.Model):
     'associates' relationship.
     """
     nickname = models.CharField(max_length=255, unique=True, null=False)
-    associates = models.ManyToManyField('self', symmetrical=True, blank=true)
+    associates = models.ManyToManyField('self', symmetrical=True, blank=True)
 
     def __str__(self):
         return self.nickname
