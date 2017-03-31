9c63cc8629f21a268e3ac0e0feef8c77faa8f6f8
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2017-03-31 11:13:12 -0400

    remove sphinx.ext.imgmath

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
