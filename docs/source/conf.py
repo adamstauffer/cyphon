d447969fc10b396f8757d24e6903fb8e7d1f1145
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2017-03-31 11:13:25 -0400

    fix path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../../django'))
+sys.path.insert(0, os.path.abspath('../../cyphon'))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'cyphon.settings.sphinx'
 import django
