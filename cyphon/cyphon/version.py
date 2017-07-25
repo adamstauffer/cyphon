811978147428c11548982a20157810d95fed1cfd
Chase Brewer <chase.brewer@dunbarcyber.com>
2017-07-25 16:50:14 -0400

    create version fetching functions and middleware

---
+++ cyphon/cyphon/version.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Dunbar Security Solutions, Inc.
+#
+# This file is part of Cyphon Engine.
+#
+# Cyphon Engine is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# Cyphon Engine is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cyphon Engine. If not, see <http://www.gnu.org/licenses/>.
+
+# standard library
+from subprocess import check_output
+import re
+
+VERSION_REGEX = re.compile(b'\d+\.\d+\.\d+')
+"""RegExp
+
+Regular express used to pull the version number of Cyphon from the most
+recent git commit tag.
+"""
+
+VERSION_HEADER = 'Cyphon-Version'
+"""str
+
+Name of the header that contains the cyphon version number.
+"""
+
+def get_version():
+    """Returns the current version of Cyclops according to it's tag on git.
+
+    Returns
+    -------
+    string or None
+    """
+    raw_version = check_output(['git', 'describe', '--tags'])
+
+    if not raw_version:
+        return None
+
+    version = VERSION_REGEX.match(raw_version)
+
+    if not version:
+        return None
+
+    return version.group(0)
+
+
+CURRENT_VERSION = get_vesrsion() or ''
+"""str
+
+Cached current version number of Cyphon created at initial runtime.
+"""
+
+
+class VersionMiddleware(object):
+    """
+    Middleware that adds version information to responses.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """Adds the Cyphon version number into a header on every response.
+
+        Parameters
+        ----------
+        request : :class: `~django.http.HttpRequest`
+
+        Returns
+        -------
+        :class: `~django.http.HttpResponse`
+        """
+        response = self.get_response(request)
+        response[VERSION_HEADER] = CURRENT_VERSION
