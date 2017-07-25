398f654eda31c84c42f52f50b535e2bcc9ff8d4f
Chase Brewer <chase.brewer@dunbarcyber.com>
2017-07-25 13:03:08 -0400

    add doc_id to alert detail serializer

--- cyphon/alerts/serializers.py
+++ cyphon/alerts/serializers.py
@@ -133,6 +133,7 @@ class AlertDetailSerializer(serializers.ModelSerializer):
             'data',
             'dispatches',
             'distillery',
+            'doc_id',
             'incidents',
             'level',
             'link',
