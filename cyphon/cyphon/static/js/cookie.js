2e6be8ebdfb0216d2e5464b9cc777932ee8180c2
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2017-07-26 09:06:19 -0400

    use single quotes

--- cyphon/cyphon/static/js/cookie.js
+++ cyphon/cyphon/static/js/cookie.js
@@ -44,7 +44,7 @@
         $.ajaxSetup({
             beforeSend: function(xhr, settings) {
                 if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                    xhr.setRequestHeader('X-CSRFToken', csrftoken);
                 }
             }
         });
