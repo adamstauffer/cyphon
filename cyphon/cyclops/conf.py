bde0cee21a76277dedc708be892ea1d90e26b3d0
Chase Brewer <chase.brewer@dunbarcyber.com>
2018-01-19 16:10:21 -0500

    update cyclops to 0.5.4

--- cyphon/cyclops/conf.py
+++ cyphon/cyclops/conf.py
@@ -20,7 +20,7 @@ Settings for the Cyclops package.
 
 CYCLOPS_AWS_BUCKET_URL = 'https://s3.amazonaws.com/cyclops-public'
 
-CYCLOPS_VERSION = '0.5.3'
+CYCLOPS_VERSION = '0.5.4'
 """str
 
 Version number of Cyclops to use.
